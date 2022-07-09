9444e651dbe0f686c371e4039e727b67f4b48938
JussiKarkkainen <juskark@gmail.com>
2022-07-09 21:41:45 +0300

    continue optim

--- core/optim.py
+++ core/optim.py
@@ -1,25 +1,65 @@
 
 class Optim:
-    def __init__(self, params):
+    def __init__(self, params, weight_decay=0):
         self.params = params
+        self.weight_decay = weight_decay
 
     def zero_grad(self):
         for param in self.params:
             param.grad = None
-    
 
-class SGD:
-    def __init__(self, params, lr):
+ 
+# https://pytorch.org/docs/stable/generated/torch.optim.SGD.html
+class SGD(Optim):
+    def __init__(self, params, lr, momentum=0, dampening=0, 
+                 nesterov=False, maximize=False):
+        super().__init__()
         self.lr = lr
-        slef.params = params
+        self.momentum = momentum
+        self.dampening = dampening
+        self.nesterov = nesterov
+        self.maximize = maximize
 
     def step(self):
         for param in self.params:
-            param -= self.lr * param.grad  
+            if self.weight_decay:
+                param.grad += self.weight_decay * param
+            # TO-DO: implement momentum
+            '''
+            if momentum:
+               if nesterov
+            '''
+            if self.maximize:
+                param += self.lr * param.grad
+            else
+                param -= self.lr * param.grad  
+
+# Values for arguments:
+# https://pytorch.org/docs/stable/generated/torch.optim.RMSprop.html
+class RMSprop(Optim):
+    def __init__(self, params, lr=0.01, alpha=0.99, eps=1e-8, 
+                 momentum=0, centered=False):
+        super().__init__()
+        self.lr = lr
+        self.alpha = alpha
+        self.eps = eps
+        self.momentum = momentum
+        self.centered = centered
 
 
-class RMSprop:
+    def step(self)
+
+
+class Adam(Optim):
+    def __init__(self, betas=(0.9, 0.999), eps=1e-8, amsgrad=False, maximize=False):
+        super().__init__()
+        self.betas = betas
+        self.eps = eps
+        self.amsgrad = amsgrad
+        self.maximize = maximize
+
+
+    def step(self):
 
 
 
-class Adam:
