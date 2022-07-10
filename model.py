f2c80e11f017b71f76052e59b410a3aea67dad46
JussiKarkkainen <juskark@gmail.com>
2022-07-10 13:01:00 +0300

    add blueprint for model class

---
+++ model.py
@@ -0,0 +1,12 @@
+
+
+class Model:
+    ''' Class for making a model '''
+    
+    def __init__(self):
+
+    def initalize_net(self):
+
+    def train(self, data, target, batch_size, num_epochs, optim, loss_fn):
+
+    def predict(self, data):
