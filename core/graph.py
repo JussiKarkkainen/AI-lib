22eeed400a45b56173c77c5e65980508c41d71a1
JussiKarkkainen <juskark@gmail.com>
2022-07-17 16:23:02 +0300

    continue graph

--- core/graph.py
+++ core/graph.py
@@ -1,4 +1,29 @@
+import ops
 
+def check_node(function, self, y):
+    if isinstance(y, Node):
+        return function(self, y)
+    raise TypeError("Incompatible type") 
+
+
+class Node:
+    def __init__(self):
+        pass
+    
+    def __add__(self, y):
+        return check_node(Add, self, y)
+
+    def __mul__(self, y):
+        return check_node(Mul, self, y)
+
+    def __div__(self, y):
+        return check_node(Div, self, y)
+
+    def __pow__(self, y):
+        return check_node(Pow, self, y)
+
+    def __matmul__(self, y):
+        return check_node(Matmul, self, y)
 
 class Graph:
     ''' Class for computational graphs
@@ -10,3 +35,4 @@ class Graph:
     
         global _graph
         _graph = self
+
