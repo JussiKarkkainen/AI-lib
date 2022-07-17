a93921f7b9bdbf32f1ea002139c058d3a1507667
JussiKarkkainen <juskark@gmail.com>
2022-07-17 16:07:51 +0300

    add bakward for ops

--- core/ops.py
+++ core/ops.py
@@ -1,4 +1,5 @@
 import graph
+import numpy as np
 
 class Ops:
     def __init__(self):
@@ -6,7 +7,6 @@ class Ops:
         self.inputs = []
         self.grad = None
         
-
 class Add(Ops):
     def __init__(self, x, y, name=None):
         super().__init__(name)
@@ -16,8 +16,8 @@ class Add(Ops):
     def forward(self, x, y):
         return x + y
 
-    def backward():
-        pass
+    def backward(self, x, y, dout):
+        return dout, dout        
 
 class Mul(Ops):
     def __init__(self, x, y, name=None):
@@ -28,20 +28,8 @@ class Mul(Ops):
     def forward(self, x, y):
         return x * y
 
-    def backward():
-        pass
-
-class Sub(Ops):
-    def __init__(self, x, y, name=None):
-        super().__init__(name)
-        self.inputs = [x, y]
-        self.name = f"sub" if name is None else name
-
-    def forward(self, x, y):
-        return x - y
-    
-    def backward():
-        pass
+    def backward(self, x, y, dout):
+        return x*dout, y*dout
 
 class Div(Ops):
     def __init__(self, x, y, name=None):
@@ -52,8 +40,8 @@ class Div(Ops):
     def forward(self, x, y):
         return x * y**-1
     
-    def backward():
-        pass
+    def backward(self, x, y, dout):
+        return dout/y, dout*x/y**2
 
 class Pow(Ops):
     def __init__(self, x, y, name=None):
@@ -63,5 +51,16 @@ class Pow(Ops):
     def forwardi(self, x, y):
         return x ** y
 
-    def backward():
-        pass
+    def backward(self, x, y, dout):
+        return dout*y*x**(y-1), dout*np.log(a)*x**Y
+
+class Matmul(Ops):
+    def __init__(self, x, y, name=None):
+        self.inputs = [x, y]
+        self.name = f"matmul" if name is None else name
+
+    def forward(self, x, y):
+        return x @ y
+
+    def backward(self, x, y, dout):
+        return x.T @ dout, y.T @ dout
