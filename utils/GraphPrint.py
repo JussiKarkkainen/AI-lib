dfbd5f299c6edd627b07aee63844342953ae1ca7
JussiKarkkainen <juskark@gmail.com>
2022-07-21 21:27:47 +0300

    Add printing function for backprop

--- utils/GraphPrint.py
+++ utils/GraphPrint.py
@@ -1,6 +1,16 @@
 from graphviz import Digraph
-
+from core.ops import Ops
 
 def print_graph(graph):
     ''' Visualizing graphs for backpropagation'''
-    pass
+    viz = DiGraph()
+    viz.attr(rankdir='LR', size='10, 8')
+    viz.attr('node', shape='circle')
+    for node in graph:
+        viz.node(node.name, label=node.name.spilt('/')[0], shape='circle')
+    for node in graph:
+        if isinstance(node, Ops):
+            for i in node.inputs:
+                viz.edge(i.name, node.name, label=i.name)
+
+    return viz
