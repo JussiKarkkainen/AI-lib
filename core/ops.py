fad8cd0c2454c2ea0d1c023b595bcecbc7b158de
JussiKarkkainen <juskark@gmail.com>
2022-07-25 20:32:30 +0300

    refactor ops for autodiff rewrite

--- core/ops.py
+++ core/ops.py
@@ -1,42 +1,6 @@
 import numpy as np
 from tensor import Tensor
 
-def check_node(function, self, y):
-    if isinstance(y, Node):
-        return function(self, y)
-    raise TypeError("Incompatible type") 
-
-
-class Node:
-    def __init__(self):
-        pass
-    
-    def __add__(self, y):
-        return check_node(Add, self, y)
-
-    def __mul__(self, y):
-        return check_node(Mul, self, y)
-
-    def __div__(self, y):
-        return check_node(Div, self, y)
-
-    def __pow__(self, y):
-        return check_node(Pow, self, y)
-
-    def __matmul__(self, y):
-        return check_node(Matmul, self, y)
-
-class Graph:
-    ''' Class for computational graphs
-        _graph is a global variable that describes the graph
-    '''
-    def __init__(self):
-        self.ops = set()
-    
-        global _graph
-        _graph = self
-    
-
 class Function:
     def __init__(self, name='Operator'):
         _graph.ops.add(self)
@@ -54,13 +18,6 @@ class Function:
         return f"Op: name: {self.name}"
 
 class ReLU(Function):
-    relu_counter = 0
-    def __init__(self, x, name=None):
-        super().__init__()
-        self.inputs = [x]
-        self.name = f"relu/num{ReLU.relu_counter}" if name is not None else name
-        ReLU.relu_counter += 1
-
     def forward(self, x):
         self.out = np.maximum(0, x)
         return self.out
@@ -70,13 +27,6 @@ class ReLU(Function):
 
 
 class Add(Function):
-    add_counter = 0
-    def __init__(self, x, y, name=None):
-        super().__init__(name)
-        self.inputs = [x, y]
-        self.name = f"add/num{Add.add_counter}" if name is None else name
-        Add.add_counter += 1
-
     def forward(self, x, y):
         return x + y
 
@@ -84,13 +34,6 @@ class Add(Function):
         return dout, dout        
 
 class Mul(Function):
-    mul_counter = 0
-    def __init__(self, x, y, name=None):
-        super().__init__(name)
-        self.inputs = [x, y]
-        self.name = f"mul/num={Mul.mul_counter}" if name is None else name
-        Mul.mul_counter += 1
-
     def forward(self, x, y):
         return x * y
 
@@ -98,13 +41,6 @@ class Mul(Function):
         return x*dout, y*dout
 
 class Div(Function):
-    div_counter = 0
-    def __init__(self, x, y, name=None):
-        super().__init__(name)
-        self.inputs = [x, y]
-        self.name = f"div/num={Div.div_counter}" if name is None else name
-        Div.div_counter += 1
-
     def forward(self, x, y):
         return x * y**-1
     
@@ -112,12 +48,6 @@ class Div(Function):
         return dout/y, dout*x/y**2
 
 class Pow(Function):
-    pow_counter = 0
-    def __init__(self, x, y, name=None):
-        self.inputs = [x, y]
-        self.name = f"pow/num={Pow.pow_counter}" if name is None else name
-        Pow.pow_counter += 1
-
     def forwardi(self, x, y):
         return x ** y
 
@@ -125,12 +55,6 @@ class Pow(Function):
         return dout*y*x**(y-1), dout*np.log(a)*x**Y
 
 class Matmul(Function):
-    matmul_counter = 0
-    def __init__(self, x, y, name=None):
-        self.inputs = [x, y]
-        self.name = f"matmul/num={Matmul.matmul_counter}" if name is None else name
-        Matmul.matmul_counter += 1
-
     def forward(self, x, y):
         return x @ y
 
