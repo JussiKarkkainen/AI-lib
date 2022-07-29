315447b4e079946260651a799cf1aec3967aff11
JussiKarkkainen <juskark@gmail.com>
2022-07-29 10:59:06 +0300

    add op processing

--- core/op_types.py
+++ core/op_types.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from backend.CpuOps import CpuOps
+from backend.GpuOps import GpuOps
 
 class OpTypes:
     def __init__(self, *x, op, device=None):
@@ -7,8 +9,25 @@ class OpTypes:
         self.op = op
     
     def binary_op(self):
-        if self.o
+        if device == "cpu":
+            return CpuOps.binary_op(self.inputs, self.op)
+        elif device == "cuda":
+            return GpuOps.binary_op(self.inputs, self.op, self.device)
+        elif device == "opencl":
+            return GPUOps.binary_op(self.inputs, self.op, self.device)
         
     def unary_op(self):
-    def tensor_op(self):
+        if device == "cpu":
+            return CpuOps.unary_op(self.inputs, self.op)
+        elif device == "cuda":
+            return GpuOps.unary_op(self.inputs, self.op, self.device)
+        elif device == "opencl":
+            return GPUOps.unary_op(self.inputs, self.op, self.device)
 
+    def tensor_op(self):
+        if device == "cpu":
+            return CpuOps.tensor_op(self.inputs, self.op)
+        elif device == "cuda":
+            return GpuOps.tensor_op(self.inputs, self.op, self.device)
+        elif device == "opencl":
+            return GPUOps.tensor_op(self.inputs, self.op, self.device)
