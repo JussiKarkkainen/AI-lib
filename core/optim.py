35a20a9b6197cdaebb47affb29ac315569bd7310
JussiKarkkainen <juskark@gmail.com>
2022-08-10 21:28:18 +0300

    RMSprop

--- core/optim.py
+++ core/optim.py
@@ -1,4 +1,3 @@
-
 class Optim:
     def __init__(self, params):
         self.params = params
@@ -10,8 +9,6 @@ class Optim:
         for param in self.params:
             param.grad = None
 
- 
-# https://pytorch.org/docs/stable/generated/torch.optim.SGD.html
 class SGD(Optim):
     def __init__(self, params, lr, momentum=0, weight_decay=0.0):
         super().__init__()
@@ -28,21 +25,18 @@ class SGD(Optim):
             v = self.momentum * self.velocity + param.grad + self.weight_decay * param.grad
             param -= self.lr * v
 
-# Values for arguments:
-# https://pytorch.org/docs/stable/generated/torch.optim.RMSprop.html
 class RMSprop(Optim):
-    def __init__(self, params, lr=0.01, alpha=0.99, eps=1e-8, 
-                 weight_decay = 0.0, momentum=0, centered=False):
+    def __init__(self, params, lr=0.01, alpha=0.99, eps=1e-8) 
         super().__init__()
         self.lr = lr
         self.alpha = alpha
         self.eps = eps
-        self.momentum = momentum
-        self.centered = centered
-
+        self.v = [Tensor.zeros(param.shape, param[0].device, requires_grad=False) for param in self.params]
 
     def step(self):
-        pass
+        for param in self.params:
+            self.v = self.alpha * self.v + (1 - self.alpha) * param.grad.pow(2)
+            param -= (self.lr * param.grad).div(self.v.pow(0.5) + self.eps)
 
 class Adam(Optim):
     def __init__(self, betas=(0.9, 0.999), eps=1e-8, amsgrad=False, maximize=False):
@@ -52,8 +46,6 @@ class Adam(Optim):
         self.amsgrad = amsgrad
         self.maximize = maximize
 
-
     def step(self):
         pass
-
 
