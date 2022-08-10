6ce8f6743394d99c515f653322a10fdef968c536
JussiKarkkainen <juskark@gmail.com>
2022-08-10 22:07:12 +0300

    add adam

--- core/optim.py
+++ core/optim.py
@@ -34,18 +34,26 @@ class RMSprop(Optim):
         self.v = [Tensor.zeros(param.shape, param[0].device, requires_grad=False) for param in self.params]
 
     def step(self):
-        for param in self.params:
-            self.v = self.alpha * self.v + (1 - self.alpha) * param.grad.pow(2)
-            param -= (self.lr * param.grad).div(self.v.pow(0.5) + self.eps)
+        for i, param in enumerate(self.params):
+            self.v[i] = self.alpha * self.v[i] + (1 - self.alpha) * param.grad.pow(2)
+            param -= (self.lr * param.grad).div(self.v[i].pow(0.5) + self.eps)
 
 class Adam(Optim):
-    def __init__(self, betas=(0.9, 0.999), eps=1e-8, amsgrad=False, maximize=False):
+    def __init__(self, lr=0.001, betas=(0.9, 0.999), eps=1e-8):
         super().__init__()
+        self.lr = lr
         self.betas = betas
         self.eps = eps
-        self.amsgrad = amsgrad
-        self.maximize = maximize
+        
+        self.m = [Tensor.zeros(param.shape, param[0].device, requires_grad=False) for param in self.params]
+        self.v = [Tensor.zeros(param.shape, param[0].device, requires_grad=False) for param in self.params]
+        self.t = 0
 
     def step(self):
-        pass
+        self.t += 1
+        for i, param in enumerate(self.params):
+            self.m[i] = self.betas[0] * self.m[i] + (1 - self.betas[0]) * param.grad
+            self.v[i] = self.betas[1] * self.v[i] + (1 - self.betas[1]) * (param.grad * param_grad)
+            param -= (self.lr * (self.m[i].div(1 - self.betas[0].pow(self.t)))).div(((self.v[i].div(1 - self.betas[1].pow(self.t))) + self.eps)) + self.eps))))
+
 
