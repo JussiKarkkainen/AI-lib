b268fdb76e78e0d073c688ad95a851e7e858ab58
JussiKarkkainen <juskark@gmail.com>
2022-08-13 22:20:09 +0300

    fix mnist loader

--- dataset/loader.py
+++ dataset/loader.py
@@ -2,18 +2,19 @@ import os
 import sys
 import numpy as np
 import gzip
+from core.tensor import Tensor
 
 def load_mnist():
-    X_train = gzip.open("../dataset/mnist/train-images-idx3-ubyte.gz").read()
-    X_train = np.frombuffer(X_train, dtype=np.uint8)[0x10:].reshape((-1, 28, 28)).astype(np.float32) 
+    train_img = gzip.open("../dataset/mnist/train-images-idx3-ubyte.gz").read()
+    train_img = Tensor(np.frombuffer(train_img, dtype=np.uint8)[0x10:].reshape((-1, 28, 28)).astype(np.float32))
 
-    Y_train = gzip.open("../dataset/mnist/train-labels-idx1-ubyte.gz").read()
-    Y_train = np.frombuffer(Y_train, dtype=np.uint8)[8:]
+    train_labels = gzip.open("../dataset/mnist/train-labels-idx1-ubyte.gz").read()
+    train_labels = Tensor(np.frombuffer(train_labels, dtype=np.uint8)[8:])
     
-    X_test = gzip.open("../dataset/mnist/t10k-images-idx3-ubyte.gz").read()
-    X_test = np.frombuffer(X_train, dtype=np.uint8).reshape((-1, 28, 28)).astype(np.float32) 
+    test_img = gzip.open("../dataset/mnist/t10k-images-idx3-ubyte.gz").read()
+    test_img = Tensor(np.frombuffer(test_img, dtype=np.uint8)[0x10:].reshape((-1, 28, 28)).astype(np.float32))
 
-    Y_test = gzip.open("../dataset/mnist/t10k-labels-idx1-ubyte.gz").read()
-    Y_test = np.frombuffer(Y_train, dtype=np.uint8)
+    test_labels = gzip.open("../dataset/mnist/t10k-labels-idx1-ubyte.gz").read()
+    test_labels = Tensor(np.frombuffer(test_labels, dtype=np.uint8)[8:])
 
-    return X_train, X_test, Y_train, Y_test
+    return train_img, train_labels, test_img, test_labels
