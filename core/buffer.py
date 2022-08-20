87161145431db203057a262d3acc1818775997df
JussiKarkkainen <juskark@gmail.com>
2022-08-20 15:55:47 +0300

    one function handles all ops

--- core/buffer.py
+++ core/buffer.py
@@ -36,7 +36,7 @@ def eval_load_op(buf:Buffer):
     assert(buf.op.op == LoadOp.fromCpu)
     return Device.buffers[buf.device].fromCpu(buf.op.arg), [], LoadOp
 
-def eval_unary_op(parents:Buffer):
+def eval_op_all(parents:Buffer, shape=None):
     real_parents = {x:None for x in parents.op.src}
     for x in real_parents.keys():
         real_parents[x] = x.eval_op(x.device)
@@ -45,51 +45,15 @@ def eval_unary_op(parents:Buffer):
             return real_parents[x]
         if isinstance(x.op, UnaryOp):
             return resolve(x.src[0]).unary_op(x.op)
-    return resolve(parents.op), list(real_parents.values()), UnaryOp
-
-def eval_reduce_op(parents:Buffer, axis):
-    real_parents = {x:None for x in parents.op.src}
-    for x in real_parents.keys():
-        real_parents[x] = x.eval_op(x.device)
-    def resolve(x:Union[Buffer, Ops]):
-        if isinstance(x, Buffer):
-            return real_parents[x]
-        if isinstance(x.op, ReduceOp):
-            return resolve(x.src[0]).reduce_op(x.op, axis)
-    return resolve(parents.op), list(real_parents.values()), ReduceOp
-
-def eval_transform_op(parents:Buffer, shape):
-    real_parents = {x:None for x in parents.op.src}
-    for x in real_parents.keys():
-        real_parents[x] = x.eval_op(x.device)
-    def resolve(x:Union[Buffer, Ops]):
-        if isinstance(x, Buffer):
-            return real_parents[x]
-        if isinstance(x.op, TransformOp):
-            return resolve(x.src[0]).transform_op(x.op, shape)
-    return resolve(parents.op), list(real_parents.values()), TransformOp
-
-def eval_tensor_op(parents:Buffer):
-    real_parents = {x:None for x in parents.op.src}
-    for x in real_parents.keys():
-        real_parents[x] = x.eval_op(x.device)
-    def resolve(x:Union[Buffer, Ops]):
-        if isinstance(x, Buffer):
-            return real_parents[x]
-        if isinstance(x.op, TensorOp):
-            return resolve(x.src[0]).tensor_op(x.op, resolve(x.src[1]))
-    return resolve(parents.op), list(real_parents.values()), TensorOp
-
-def eval_binary_op(parents:Buffer):
-    real_parents = {x:None for x in parents.op.src}
-    for x in real_parents.keys():
-        real_parents[x] = x.eval_op(x.device)
-    def resolve(x:Union[Buffer, Ops]):
-        if isinstance(x, Buffer):
-            return real_parents[x]
         if isinstance(x.op, BinaryOp):
             return resolve(x.src[0]).binary_op(x.op, resolve(x.src[1]))
-    return resolve(parents.op), list(real_parents.values()), BinaryOp
+        if isinstance(x.op, ReduceOp):
+            return resolve(x.src[0]).reduce_op(x.op, shape)
+        if isinstance(x.op, TensorOp):
+            return resolve(x.src[0]).tensor_op(x.op, resolve(x.src[1]))
+        if isinstance(x.op, TransformOp):
+            return resolve(x.src[0]).transform_op(x.op, shape)
+    return resolve(parents.op), list(real_parents.values()), parents.op_type
 
 _eval = {LoadOp: eval_load_op, BinaryOp: eval_binary_op, UnaryOp: eval_unary_op, TensorOp: eval_tensor_op}
 
@@ -111,17 +75,17 @@ class Buffer:
         assert x.device == y.device
         src = tuple(x.op if x.op_type == BinaryOp else i for i in tuple([x, y]))
         buf = Buffer(Ops(op, src), BinaryOp, x.device)
-        return eval_binary_op(buf)[0]
+        return eval_op_all(buf)[0]
 
     def unary_op(self, op):
         src = tuple(self.op if self.op_type == UnaryOp else i for i in tuple([self]))
         buf = Buffer(Ops(op, src), UnaryOp, self.device)
-        return eval_unary_op(buf)[0]
+        return eval_op_all(buf)[0]
    
     def reduce_op(self, op, axis):
         src = tuple(self.op if self.op_type == ReduceOp else i for i in tuple([self]))
         buf = Buffer(Ops(op, src), ReduceOp, self.device)
-        return eval_reduce_op(buf, axis)[0] 
+        return eval_op_all(buf, axis)[0] 
 
     def transform_op(self, op, shape, return_buf=False):
         if shape == self.op.arg.shape and (op == TransformOp.Reshape or op == TransformOp.Expand):
@@ -132,14 +96,14 @@ class Buffer:
         buf = Buffer(Ops(op, src), TransformOp, self.device)
         if return_buf:
             return Buffer.fromCpu(eval_transform_op(buf, shape)[0], self.device)
-        return eval_transform_op(buf, shape)[0]
+        return eval_op_all(buf, shape)[0]
 
     def tensor_op(x, op, y):
         if type(y) == np.ndarray:
             y = Buffer.fromCpu(y, x.device)
         src = tuple(x.op if x.op_type == TensorOp else i for i in tuple([x, y]))
         buf = Buffer(Ops(op, src), TensorOp, x.device)
-        return eval_tensor_op(buf)[0]
+        return eval_op_all(buf)[0]
 
     def eval_op(self, device=None):
         if device is not None:
