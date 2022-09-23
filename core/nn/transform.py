74a3ae94b29916e88c796a29bc532b8d86f6b744
JussiKarkkainen <juskark@gmail.com>
2022-09-23 19:42:23 +0300

    well, convnet runs without errors, don't know if correctly though

--- core/nn/transform.py
+++ core/nn/transform.py
@@ -8,7 +8,8 @@ class Context:
 
     def get_params(self):
         return self._params
-
+    def get_init_state(self):
+        return self._state
     def get_state(self):
         return self._state
 
@@ -31,7 +32,10 @@ class Transformed:
         self.init = init_fn
         self.apply = apply_fn
 
-
+class TransformedState:
+    def __init__(self, init_fn, apply_fn):
+        self.init = init_fn
+        self.apply = apply_fn
 
 def transform(f):
     ''' 
@@ -39,17 +43,44 @@ def transform(f):
     pair of pure functions, init and apply. Works in the same 
     way as hk.transform in Haiku.
     '''
-    def init_fn(x_init, y_init):
+    return without_state(transform_with_state(f))
+
+def without_state(f):
+    def init_fn(*args, **kwargs):
+        params, state = f.init(*args, **kwargs)
+        if state:
+            raise ValueError("Can't have state in init")
+        return params
+
+    def apply_fn(params, *args, **kwargs):
+        if "state" in kwargs:
+            raise TypeError("State in kwargs")
+        out, state = f.apply(params, {}, *args, **kwargs)
+        if state:
+            raise ValueError("Can't have state in apply")
+        return out
+
+    tie_fn(f, init_fn, apply_fn)
+
+    return Transformed(init_fn=init_fn, apply_fn=apply_fn)
+
+def transform_with_state(f):
+    
+    def init_fn(*args, **kwargs):
         with new_ctx() as ctx:
             try:
-                f(x_init, y_init)
+                f(*args, **kwargs)
             except Exception as e: 
                 print(e)
-        return ctx.get_params(), ctx.get_state()
+        return ctx.get_params(), ctx.get_init_state()
 
-
-    def apply_fn():
-        pass
+    def apply_fn(params, state, *args, **kwargs):
+        with new_ctx(params=params, state=state) as ctx:
+            try:
+                out = f(*args, **kwargs)
+            except Exception as e:
+                print(e)
+        return out, ctx.get_state()
 
     tie_fn(f, init_fn, apply_fn)
 
@@ -57,5 +88,16 @@ def transform(f):
 
 
 def tie_fn(f, init_fn, apply_fn):
-    pass
+    if isinstance(f, Transformed):
+        f = getattr(f.init, "_original_fn")
+    init_fn._original_fn = f
+    apply_fn._original_fn = f
+
+
+
+
+
+
+
+
 
