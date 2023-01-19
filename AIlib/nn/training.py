ee232caf892582bec51f15a65d16c40b79198d88
Jussi Karkkainen <juskark@gmail.com>
2023-01-19 13:58:17 +0200

    fix training loop

--- AIlib/nn/training.py
+++ AIlib/nn/training.py
@@ -3,35 +3,30 @@ import core.nn as nn
 from core.tensor import Tensor
 from core.autograd import grad
 
-def train(lossfn, optim, lr, train_loader, ):
+def train(net_fn, optim, dataset_loader, num_epochs, one_hot=False):
     network = transform(net_fn)
     optimizer = optim
     
-    def loss(params, X, y):
+    def loss_fn(params, X, y):
         out = network.apply(params, X)
         out = lossfn(out, y)
         return out
 
-    def update(state, X, y):
-        grads = grad(loss)(state.params, X, y)
+    def update_weights(params, X, y):
+        grads = grad(loss)(params, X, y)
         params, opt_state = optimizer.update(grads, state.opt_state)
-        return nn.TrainingState(params, opt_state)
+        return nn.TrainingState(params, opt_state), loss
    
-    def evaluate(params, X, y):
-        out = network.apply(params, X, y)
-        predictions = np.argmax(out, axis=-1)
-        return Tensor.mean(predictions == y)
-
-    train_loader, x_init, y_init = load_dataset()
-    init_params = network.init(x_init.flatten())
+    train_loader, x_init = dataset_loader()
+    init_params = network.init(x_init)
     init_opt_state = optimizer.init(init_params)
     state = nn.TrainingState(params=init_params, opt_state=init_opt_state)
 
-    for epoch in range(10):
+    for epoch in range(num_epochs):
+        epoch_loss = 0
         for X, y in train_loader:
             X = Tensor(np.array(X)).flatten()
-            y = Tensor(np.array(y))
-            state = update(state, X, y)
-
-        accuracy = evaluate(state.params, X, y)
-        print(f"epoch: {epoch}, accuracy: {accuracy:.3f}")
+            y = nn.utils.one_hot(Tensor(np.array(y)), 10).detach() if one_hot else Tensor(np.array(y))
+            state, loss = update_weights(state.params, X, y)
+            epoch_loss += loss
+        print(f"epoch: {epoch}, loss: {epoch_loss:.3f}")
